031640edd07423c3a703544baef067ccedd5df33
OrkideMaden <65h2yw5pbb@privaterelay.appleid.com>
2026-03-22 23:29:41 -0500

    Add test report screenshot to README

--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -1,23 +1,33 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import { TodoPage } from '../pages/TodoPage';
 import data from '../test-data/todos.json';
 
-test('full todo scenario: add → complete → filter → delete', async ({ page }) => {
+const todos = Object.values(data);
 
+test.beforeEach(async ({ page }) => {
   const todoPage = new TodoPage(page);
-
-  // Add
   await todoPage.goto();
-  await todoPage.addTodo(data.todo1);
-  await todoPage.expectTodoVisible(data.todo1);
-  // Complete
-  await todoPage.completeTodoByText(data.todo1);
-  // Filter Completed
-  await todoPage.filterCompleted();
-  await todoPage.expectTodoVisible(data.todo1);
-  // Delete
-  await todoPage.deleteTodoByText(data.todo1);
-  // Final assert: there is no item in the list
-  await todoPage.expectTodoCount(0);
-
 });
+
+for (const todoText of todos) {
+  test(`full todo scenario: ${todoText}`, async ({ page }) => {
+    const todoPage = new TodoPage(page);
+
+    // Add
+    await todoPage.addTodo(todoText);
+    await todoPage.expectTodoVisible(todoText);
+
+    // Complete
+    await todoPage.completeTodoByText(todoText);
+
+    // Filter Completed
+    await todoPage.filterCompleted();
+    await todoPage.expectTodoVisible(todoText);
+
+    // Delete
+    await todoPage.deleteTodoByText(todoText);
+
+    // Final assert
+    await todoPage.expectTodoCount(0);
+  });
+}
